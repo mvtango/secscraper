9ffeeada9eaf32a92d4d4184b753630284c3a8cc
Martin Virtel <martin.virtel@gmail.com>
2013-07-18 03:42:00 +0200

    initial

---
+++ nq-extract.py
@@ -0,0 +1,154 @@
+import simplejson
+import pprint
+import re
+from scraper.scraperlib import TreeScraper,TextEditor,TextParser
+
+unifier= [
+	[r"^[0-9,]+$", lambda a: a.string.replace(",","") ],
+	["\s\s+", " "],
+	["^\s+", "" ],
+	["\s+$", "" ],
+	["Date of reporting period: (\d\d)/(\d\d)/(\d\d\d\d)", r"\3-\2-\1"],
+	["\s+\(concluded\)", "" ],
+	["\s+\(continued\)", "" ],
+	{ 're' : re.compile("\s*\x97.*",re.DOTALL), 'repl' : "" },
+	{ 're' : re.compile("\s*\x96.*",re.DOTALL), 'repl' : "" },
+	{ 're' : re.compile(", Inc.:?"), 'repl' : "" },
+	{ 're' : re.compile("^\xa0"), 'repl' : "" },
+	{ 're' : re.compile("^\$"), 'repl' : "" },
+
+]
+
+
+files={ "in" : [
+				"data/d/d538216dnq.htm",
+				"data/d/i00449_if-nq.htm",
+				"data/d/i00222_if-nq.htm",
+				"data/d/i00499_if-nq.htm",
+				"data/d/i00242_ifi-nq.htm",
+				"data/d/i00001_indexfunds-nqa.htm",
+				
+
+		],
+        "csv" : "extract.csv" }
+
+
+parse={
+       "container" : "//table",
+       "properties" : {
+			"date" : "//p/font[contains(text(),'Date of reporting period')]/text()",
+			"fund" : [ "//table[1]/tr/td//p/font[contains(text(),'Name of Fund')]/../../../../tr[1]/td[2]/p/font/text()",
+					   "//p/font[contains(text(),'Name of Fund:')]/text()" ]
+       },
+       "row" : ".//tr",
+       "columns" : {
+				"category" : "./td[1]//font[@size='2']/b/text()",
+				"name" : "./td[1]//font[@size='2']/text()",
+				"shares" : "./td[4]//font[@size='2']/text()",
+				"value" : ["./td[7]//font[@size='2']/text()", "./td[8]//font[@size='2']/text()"] ,
+
+      },
+      "ok" : [
+				"name|shares|value",
+
+      ],
+      "table" : [ "fund","date","category","name","shares","value"]
+      
+}
+
+
+
+
+def extract(e,p) :
+	if type(p)==type("") :
+		p=[p]
+	for pp in p :
+		try :
+			a=e.xpath(pp)
+			if a: 
+				return a
+		except Exception, e:
+			print "pp %s - unmatched" % pp
+			pass
+	return None
+
+def stringstring(a) :
+	try :
+		s=etree.tostring(a)
+	except TypeError :
+		if type(a) == type([]) :
+			s="".join(a)
+		else :
+			s=a
+	for u in unifier :
+		try :
+			s=u['re'].sub(u["repl"],s)
+		except Exception, e:
+			pass
+	return s
+
+
+
+def output_ok(h,fields) :
+	fs=fields.split("|")
+	return set(fs)==set(filter(lambda a: h[a],h.keys()))
+	
+	
+def output_row(h,row) :
+	rrow=[]
+	for f in row :
+		if f in h:
+			rrow.append(h[f])
+		else :
+			rrow.append("")
+	return rrow
+
+
+
+ofile=UnicodeWriter(open(files["csv"],"w"))
+for fn in files["in"] :
+	tree=etree.parse(open(fn),etree.HTMLParser())
+
+	data=[]
+	props={}
+	for (k,v) in parse["properties"].items() :
+		props[k]=stringstring(extract(tree,v))
+	pprint.pprint(props)
+	for container in tree.xpath(parse["container"]) :
+		cdata=[]
+		for row in container.xpath(parse["row"]) :
+			rd={}
+			# print "row: %s" % (etree.tostring(row)[:10])
+			for (k,x) in parse["columns"].items() :
+				if type(x) != type([]) :
+					x=[x]
+				a=[]
+				for xx in x :
+					a.extend(row.xpath(xx))
+				if a:
+					# print "matched"
+					rd[k]="".join([stringstring(ss) for ss in a])
+					if len(rd[k]) == 1 :
+						rd[k]=rd[k][0]
+			of=False
+			props.update(rd)
+	
+			for o in parse["ok"] :
+				if output_ok(rd,o) :
+					cdata.append(output_row(props,parse["table"]))
+					of=True
+					break
+	#		if not of:
+	#			rd.update({"file" : "o"})
+	#			cdata.append(output_row(rd,parse["table"]))
+				
+		data.append(cdata)
+		
+
+	# pprint.pprint(map(lambda a:  { "a" : a.get("reaction","-"), "b" : a.get("color",""), "c" : a.get("amendment","") },data));
+	print "%s containers\n" % len(data)
+	for container in data :
+		if len(container)>0 :
+			pprint.pprint(container)
+			ofile.writerows(container)
+			
---
+++ scraper/scrapelib.py
@@ -0,0 +1,119 @@
+from lxml import etree 
+from lxml.cssselect import CSSSelector, SelectorSyntaxError
+
+import requests
+import re
+
+class AddToChain :
+    
+	def __add__(self,o) :
+        	def a(*args,**kwargs) :
+            		return self(o(*args,**kwargs))
+        	return a
+
+
+
+class TextEditor(AddToChain):
+	"""
+	Takes List of Search / Replace Expressions
+	d=TextEditor([("a","b"),("c","d")])
+
+	d.process("maca") -> "mbdb"
+	"""
+	
+	def __init__(self, ruleset) :
+		self.ruleset=[(re.compile(a[0]),a[1]) for a in ruleset]
+
+
+	def edit(self,i) :
+		for r in self.ruleset :
+			i=r[0].sub(r[1],i)
+		return i 
+
+	def __call__(self,i) :
+		return self.edit(i)
+
+
+
+
+class TextParser(AddToChain) :
+	"""
+	Takes List of Regexp. Will return groupdict() of first match
+	
+	"""
+
+
+	def __init__(self, ruleset) :
+		self.ruleset=[re.compile(a) for a in ruleset]
+
+
+	def parse(self,i) :
+		for r in self.ruleset :
+			m=r.search(i)
+			if m :
+				return m.groupdict()
+		return None 
+
+
+	def __call__(self,i) :
+		return self.parse(i)
+
+
+
+class TreeScraper :
+	
+
+	def __init__(self, ss) :
+		if ss[0]=="<" :
+			self.tree=etree.fromstring(ss,etree.HTMLParser())
+		else :
+			self.tree=etree.parse(url,etree.HTMLParser())
+
+
+	def select(self,p) :
+		try :
+			sel=CSSSelector(p)
+			return sel(self.tree)
+		except SelectorSyntaxError :
+			return self.tree.xpath(p) 
+
+
+	def extract(self,xp,processor=None) :
+		r=self.select(xp)
+		if type(r) == type("") :
+			r=[r]
+		if processor :
+			res=[ processor(etree.tostring(a)) for a in r ]	
+		else :
+			res=[ etree.tostring(a) for a in r ]	
+		return res
+		
+
+		
+
+
+
+
+
+if __name__=="__main__" :
+
+	chinese=TextEditor([("r","l"),("a","m")])
+	assert chinese.edit("roaring")=="lomling"
+
+	firstlast=TextParser([r"(?P<first>\d+)(?P<last>.+)",r"(?P<last>\D+)(?P<first>\d+)"])
+	assert firstlast.parse("3a")=={ "first":"3", "last" : "a"}
+	assert firstlast("a3")=={ "first":"3", "last" : "a"}
+
+	t=TreeScraper("<html><h1>Headline</h1><p>Hallo Welt</p></html>")
+
+	assert t.select("//h1/text()")==['Headline']
+	assert t.extract("h1")==['<h1>Headline</h1>']
+
+	notags=TextEditor([["<[^>]+>",""]])
+
+	print t.extract("h1",notags)
+	print t.extract("h1",chinese)
+	print t.extract("h1",chinese+notags)
+	
+
+
